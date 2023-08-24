b0019ee204030aff9286b791c6ef8309c80c0329
Kadir GÖNEN <kadirgnen3@gmail.com>
2023-08-24 12:18:07 +0300

    Update to day2.py
    Update to day2.py

--- day2.py
+++ day2.py
@@ -7,3 +7,11 @@ print(vade+12)
 print(type(faiz))
 print(type(vade))
 print(type(krediAdi))
+
+faiz= 1.59
+vade= "36"
+krediAdi= "İhtiyaç Kredisi"
+
+print(int(vade)+12)
+#print(int(krediAdi))
+print(str(faiz))
