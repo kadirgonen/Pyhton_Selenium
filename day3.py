de3355ad91c287c54af02f3551c5af99f0e2eb65
Kadir GÖNEN <kadirgnen3@gmail.com>
2023-08-30 15:22:37 +0300

    Add to class examples
    Add to class examples

---
+++ day3.py
@@ -0,0 +1,13 @@
+#sınıflar => classlar
+#modules
+#paket yönetimi
+
+class Human:
+    def talk(self):
+        print("Human is talking..")
+    def walk():
+        print("Human is walking..")
+
+#instance => örnek
+human1= Human()
+human1.talk()
