1b53ace80b68e313fadb7022ff5573338a875628
Kadir GÖNEN <kadirgnen3@gmail.com>
2023-08-31 16:42:08 +0300

    Add to new examples
    Add to new examples

--- modules.py
+++ modules.py
@@ -1,3 +1,24 @@
 import matematik
 
 print(matematik.topla(10,20))
+
+print("**************")
+
+#alias
+import matematik as deneme
+#built-in modules
+import random
+#package
+
+print(deneme.topla(10,20))
+
+print(random.randint(0,100))
+
+print("**************")
+
+from matematik import topla as toplamaIslemi
+from day2 import sayHello
+
+print(toplamaIslemi(10,20))
+
+print(random.randint(0, 100))
