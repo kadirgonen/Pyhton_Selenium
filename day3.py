9cdfe332cacbcb4fcf026eadca4935f433da959e
Kadir GÖNEN <kadirgnen3@gmail.com>
2023-08-31 15:19:56 +0300

    Update to day3.py
    Update to day3.py

--- day3.py
+++ day3.py
@@ -12,3 +12,67 @@ class Human:
 human1= Human()
 human1.talk()
 human1.walk()
+
+print("**************")
+
+class Human:
+    def talk(self,sentence):
+        print(f"Human: {sentence}")
+    def walk(self):
+        print("Human is walking..")
+
+#instance => örnek
+human1= Human()
+human1.talk("Merhaba")
+human1.walk()
+
+print("**************")
+
+class Human:
+    name = "Halit"
+    def talk(self,sentence):
+        name= "Ercan"
+        print(f"{name}: {sentence}")
+    def walk(self):
+        print("Human is walking..")
+
+#instance => örnek
+human1= Human()
+human1.talk("Merhaba")
+human1.walk()
+
+print("**************")
+
+class Human:
+    name = "Halit"
+    def talk(self,sentence):
+        name= "Ercan"
+        print(f"{self.name}: {sentence}")
+    def walk(self):
+        print("Human is walking..")
+
+#instance => örnek
+human1= Human()
+human1.talk("Merhaba")
+human1.walk()
+
+print("**************")
+
+class Human:
+    name = "Halit"
+    def talk(self,sentence):
+        name= "Ercan"
+        print(f"{self.name}: {sentence}")
+    def walk(self):
+        print("Human is walking..")
+
+#instance => örnek
+human1= Human()
+human1.name ="Enes"
+human1.talk("Merhaba")
+human1.walk()
+
+human2= Human()
+human2.name = "Cem"
+human2.talk("Merhaba")
+human2.walk()
