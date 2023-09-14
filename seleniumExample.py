6c758a924c6939a735072e3bd1b86a7da1e50999
Kadir GÖNEN <kadirgnen3@gmail.com>
2023-09-14 14:06:31 +0300

    Update to seleniumExample.py file
    Update to seleniumExample.py file

--- seleniumExample.py
+++ seleniumExample.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from time import sleep
 
+options = Options()
+# İstediğiniz seçenekleri burada ayarlayabilirsiniz, örneğin:
+# options.add_argument("--headless")
 
-driver= webdriver.Chrome(ChromeDriverManager().install)
+driver= webdriver.Chrome(ChromeDriverManager().install(), options=options)
 driver.maximize_window()
 driver.get("https://www.google.com/")
 #sleep(10)
