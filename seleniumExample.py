be084a30227af4b54b2fae00ead2207857962ff2
Kadir GÖNEN <kadirgnen3@gmail.com>
2023-09-14 14:15:43 +0300

    Update to seleniumExample.py file
    Update to seleniumExample.py file

--- seleniumExample.py
+++ seleniumExample.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService
 from time import sleep
 
 options = Options()
@@ -9,7 +10,7 @@ options = Options()
 
 ##driver= webdriver.Chrome(ChromeDriverManager().install(), options=options)
 driver_path = ChromeDriverManager().install()  # ChromeDriver'ı indirip yükler
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+driver = webdriver.Chrome(executable_path=driver_path, options=options)
 driver.maximize_window()
 driver.get("https://www.google.com/")
 #sleep(10)
